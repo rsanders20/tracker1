581b0c3cb6c12c0ff62375dbc4958591965cf800
Ryan Sanders <rsanders20@gmail.com>
2018-06-09 08:20:32 -0500

    Added editting functionality.

--- app/src/main/java/com/example/android/tracker1/EditorActivity.java
+++ app/src/main/java/com/example/android/tracker1/EditorActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.tracker1;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import java.net.URI;
 
 /**
  * Created by ryan on 5/14/18.
@@ -9,10 +13,24 @@ import android.support.v7.app.AppCompatActivity;
 
 public class EditorActivity extends AppCompatActivity {
 
+    private Uri currentUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        Intent intent = getIntent();
+
+        currentUri = intent.getData();
+
+        if(currentUri==null) {
+            setTitle("Add record");
+        } else {
+            setTitle("Edit record");
+        }
+
+
     }
 
 
